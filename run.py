174f131a8d66770a6b53656fabe821cc3c84f7c4
domsq <Domsq@hotmail.com>
2021-08-12 17:46:58 +0000

    Add ascii art and number scenarios

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ current_scenario_index = -1
 # Scenario content moved to list as discussed with mentor
 SCENARIOS = [
   {
-    "question": "\nYou start the drive to your holiday accommodation and " +
+    "question": "\n1. You start the drive to your holiday accommodation and " +
                 "after driving\nfor a few miles " +
                 "you encounter some slow traffic. " +
                 "\nThe driver behind you seems to be following a bit too " +
@@ -15,9 +15,9 @@ SCENARIOS = [
                 "between you\nand the car in front\n" +
                 "\nB: Move over towards a position a bit left " +
                 "of the centre line of the road\n",
-    "correct_answer": "\nWell done, this is the safe way to handle " +
+    "correct_answer": "\n>Well done, this is the safe way to handle " +
                       "the situation.",
-    "wrong_answer": "\nSorry, this is the wrong answer. " +
+    "wrong_answer": "\n>Sorry, this is the wrong answer. " +
                     "The driver behind attempts " +
                     "\nto undertake you and collides " +
                     "with you in the process.\n" +
@@ -28,14 +28,15 @@ SCENARIOS = [
     "correct_choice": "a"
   },
   {
-    "question": "\nThe traffic improves but it starts to rain quite heavily." +
+    "question": "\n2. The traffic improves but it starts to rain " +
+                "quite heavily." +
                 " You are \ntravelling behind another vehicle, how much of a" +
                 " time gap should you allow?\n" +
                 "\nA: Three Seconds\n" +
                 "\nB: Four Seconds\n",
-    "correct_answer": "\nCorrect - you need to allow extra distance on " +
+    "correct_answer": "\n>Correct - you need to allow extra distance on " +
                       "slippery and wet roads.",
-    "wrong_answer": "\nNo, you have selected the wrong answer. " +
+    "wrong_answer": "\n>No, you have selected the wrong answer. " +
                     "The vehicle ahead " +
                     "brakes \nsuddenly and despite your best " +
                     "efforts, you’re unable " +
@@ -48,7 +49,7 @@ SCENARIOS = [
     "correct_choice": "b"
   },
   {
-    "question": "\nThe weather improves and it becomes dry and warm. " +
+    "question": "\n3. The weather improves and it becomes dry and warm. " +
                 "You are driving" +
                 "\non a 50mph road at the speed limit and you spot " +
                 "some stationary " +
@@ -56,11 +57,11 @@ SCENARIOS = [
                 "allow for stopping?\n" +
                 "\nA: 53m / 175 ft\n" +
                 "\nB: 36m / 120 ft\n",
-    "correct_answer": "\nWell done, this is the average stopping " +
+    "correct_answer": "\n>Well done, this is the average stopping " +
                       "distance from 50 mph." +
                       " Of course\nit’s always better to allow more " +
                       "than you think.",
-    "wrong_answer": "\nSorry, this is not the right answer. You " +
+    "wrong_answer": "\n>Sorry, this is not the right answer. You " +
                     "misjudge the " +
                     "distance to the \nvehicles ahead and are " +
                     "unable to stop in " +
@@ -73,16 +74,17 @@ SCENARIOS = [
     "correct_choice": "a"
    },
   {
-    "question": "\nYou are now travelling on a dual carriageway. There is " +
+    "question": "\n4. You are now travelling on a dual carriageway. " +
+                "There is " +
                 "a slow vehicle \nin front of you and you are " +
                 "considering overtaking" +
                 " them on the left. \nIs this allowed?\n" +
                 "\nA: Yes, it’s okay to overtake a slower vehicle in the " +
                 "right-hand lane of a \ndual carriageway\n" +
                 "\nB: No, you can only do this on a one-way street\n",
-    "correct_answer": "\nCorrect, it is okay to overtake on the left" +
+    "correct_answer": "\n>Correct, it is okay to overtake on the left" +
                       " when on a one-way street.",
-    "wrong_answer": "\nIncorrect - as you attempt to pass the " +
+    "wrong_answer": "\n>Incorrect - as you attempt to pass the " +
                     "vehicle the driver" +
                     " has started \nmoving over to the left to allow " +
                     "you past and" +
@@ -95,7 +97,7 @@ SCENARIOS = [
     "correct_choice": "b"
     },
   {
-    "question": "\nYou are now driving through some countryside on a " +
+    "question": "\n5. You are now driving through some countryside on a " +
                 "road that has " +
                 "some high \nhedges on either side of it." +
                 " You come up to a left-hand bend" +
@@ -104,10 +106,10 @@ SCENARIOS = [
                 " caused\nby the hedges. What should you watch out for?\n" +
                 "\nA: Pedestrians coming towards you\n" +
                 "\nB: No sign to warn of the bend\n",
-    "correct_answer": "\nCorrect, if the road has no footpaths " +
+    "correct_answer": "\n>Correct, if the road has no footpaths " +
                       "or pavements\n" +
                       "pedestrians will need to walk facing traffic.",
-    "wrong_answer": "\nThis is not quite the right answer. You don’t " +
+    "wrong_answer": "\n>This is not quite the right answer. You don’t " +
                     "see some " +
                     "pedestrians \napproaching and collide with them, " +
                     "injuring them as a result.\n" +
@@ -119,7 +121,7 @@ SCENARIOS = [
     "correct_choice": "a"
     },
   {
-    "question": "\nThe road visibility improves but you need to " +
+    "question": "\n6. The road visibility improves but you need to " +
                 "make a right turn into a \nside road and have a " +
                 "queue of traffic " +
                 "behind you. Why is it a good idea to \ncheck your " +
@@ -127,12 +129,12 @@ SCENARIOS = [
                 "the right before you turn?\n" +
                 "\nA: To see if anyone is trying to overtake you\n" +
                 "\nB: To see whether the side road is clear\n",
-    "correct_answer": "\nCorrect, you may have a driver trying to " +
+    "correct_answer": "\n>Correct, you may have a driver trying to " +
                       "overtake you, even " +
                       "though \nthey should go around you on the left" +
                       " if you’re " +
                       "turning right...",
-    "wrong_answer": "\nNo, this is the wrong answer. You start turning and " +
+    "wrong_answer": "\n>No, this is the wrong answer. You start turning and " +
                     "out of nowhere" +
                     "\na car who was trying to overtake you (even " +
                     "though this " +
@@ -145,17 +147,18 @@ SCENARIOS = [
     "correct_choice": "a"
     },
   {
-    "question": "\nYou notice that it has started raining again. You spot " +
+    "question": "\n7. You notice that it has started raining again. " +
+                "You spot " +
                 "a hazard ahead \nthat you need to slow down for and " +
                 "attempt to brake " +
                 "but your \nanti-lock brakes don’t seem to be working. " +
                 "Your car starts to skid. \nWhat should you do next?\n"
                 "\nA: Use the handbrake to see if this helps\n"
                 "\nB: Release the brake pedal\n",
-    "correct_answer": "\nCorrect, you’d need to initially release the " +
+    "correct_answer": "\n>Correct, you’d need to initially release the " +
                       "brake pedal to stop the skid \nand then try " +
-                      "carefully applying it again.",
-    "wrong_answer": "\nWrong, this causes your car to go into a sideways " +
+                      "to carefully apply it again.",
+    "wrong_answer": "\n>Wrong, this causes your car to go into a sideways " +
                     "slide and you \ncollide with another car.\n" +
                     "\nRule #119 of the Highway Code (pg.38, 2015 " +
                     "Edition) states: \nIf skidding occurs, remove the " +
@@ -164,17 +167,17 @@ SCENARIOS = [
     "correct_choice": "b"
     },
   {
-    "question": "\nYou start driving through a tunnel and notice what " +
+    "question": "\n8. You start driving through a tunnel and notice what " +
                 "seems to be a \nburning smell coming from your car. The " +
                 "car isn’t showing any warning lights\nand still seems " +
                 "to be driveable, what should you do?\n" +
                 "\nA: Drive it out of the tunnel if you can \n" +
                 "\nB: Pull up, then walk to an emergency phone\n",
-    "correct_answer": "\nThis is the safest course of action, well done. " +
+    "correct_answer": "\n>This is the safest course of action, well done. " +
                       "You get your car out \nof the tunnel, find a safe " +
                       "place to pull over and contact your " +
                       "roadside \nassistance company. ",
-    "wrong_answer": "\nThis isn’t the right thing to do... your car bursts " +
+    "wrong_answer": "\n>This isn’t the right thing to do... your car bursts " +
                     "into flames \ncausing a serious issue. \n" +
                     "\nRule #123 of the Highway Code (pg. 40, 2015 " +
                     "Edition) states: \nDo not leave a parked vehicle " +
@@ -182,16 +185,18 @@ SCENARIOS = [
     "correct_choice": "a"
     },
   {
-    "question": "\nYou are back on the road again and you suddenly realise " +
+    "question": "\n9. You are back on the road again and you " +
+                "suddenly realise " +
                 "you’re travelling\nagainst the flow of traffic. What’s " +
                 "the best thing to do?\n" +
                 "\nA: Perform a three-point turn to get yourself facing " +
                 "the right way\n" +
                 "\nB: Pull into a side road and turn around\n",
-    "correct_answer": "\nYes, this would be the safest option. You turn off " +
-                      "into a side road\nget safely turned around and " +
+    "correct_answer": "\n>Yes, this would be the safest option. "
+                      "You turn off into a side road," +
+                      "\nget safely turned around and " +
                       "continue on your way. ",
-    "wrong_answer": "\nThis is not the thing to do... while executing " +
+    "wrong_answer": "\n>This is not the thing to do... while executing " +
                     "a three-point " +
                     "turn \na vehicle doesn’t see you in time and " +
                     "collides with you.\n" +
@@ -202,17 +207,17 @@ SCENARIOS = [
     "correct_choice": "b"
   },
   {
-    "question": "\nYou are nearly at your seaside destination and need " +
+    "question": "\n10. You are nearly at your seaside destination and need " +
                 "to make a left turn \ninto a side road. What should you be " +
                 "watching out for most?\n" +
                 "\nA: Vehicles parked on either side\n" +
                 "\nB: Pedestrians\n",
-    "correct_answer": "\nCorrect, you’ll want to keep a watch for any " +
+    "correct_answer": "\n>Correct, you’ll want to keep a watch for any " +
                       "pedestrians. You notice \na pedestrian enter the road" +
                       " as you turn down it and avoid them successfully.\n" +
                       "\nYou have reached your destination and are ready " +
                       "to begin your holiday!",
-    "wrong_answer": "\nWhile you should always be on the lookout for other " +
+    "wrong_answer": "\n>While you should always be on the lookout for other " +
                     "vehicles, it’s \npedestrians you need to keep an eye " +
                     "out for here. You fail to see a\npedestrian step into " +
                     "the road and collide with them, causing them " +
@@ -244,10 +249,36 @@ def game_start():
     """
     Function to control start of game and display opening messages
     """
+    print("███████████████████████████████████████▀███\n"
+          "█▄─▄▄▀█▄─▄▄▀█▄─▄█▄─█─▄█▄─▄█▄─▀█▄─▄█─▄▄▄▄███\n"
+          "██─██─██─▄─▄██─███▄▀▄███─███─█▄▀─██─██▄─███\n"
+          "▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▀▀▀▄▀▀▀▄▄▄▀▄▄▄▀▀▄▄▀▄▄▄▄▄▀▀▀\n"
+          "█████████████████████████████████████\n"
+          "█─▄▄▄▄██▀▄─██▄─▄▄─█▄─▄▄─█▄─▄███▄─█─▄█\n"
+          "█▄▄▄▄─██─▀─███─▄████─▄█▀██─██▀██▄─▄██\n"
+          "▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀\n")
     print("Welcome to Driving Safely. The aim is to get to your "
           "destination\nwithout incident. "
-          "You’ll be presented with scenarios along the way\nchoose wisely"
+          "You’ll be presented with scenarios along the way\nso choose wisely"
           " as the wrong choice could end badly.\n\nEnjoy and have fun!\n")
+    time.sleep(1)
+    print("                    *,,**           *,,,,.                  \n"
+          "              ,,***,*,*,*##%%%%%%%%%(*,******,,             \n"
+          "             ,,*******,**.*,      ** **,********            \n"
+          "             **********,,*         ,,,**********,           \n"
+          "           (&&#,,****,*               ,,,,,,******          \n"
+          "         ,&&   &&,                         ,*,,,,, ..       \n"
+          "     .&&... ,%&/,     *%%##%%%%%%%%##%#,      *... &&&&&    \n"
+          "   &&&&&&&& ..##%%%%%%%%%%%%#(##(#%%%%%%%%%%%%#/#&&&&&&&&&# \n"
+          "*&&&&&&&&&&&%%%%%#(#%%%%%%%########%%%%%%%###%%%%&&&&&&&&&&&\n"
+          "&&&&&&&&&&,%%%%%%%%%%%%%%%%########%%%%%%%%%#%%%%%%&&&&&&&&&\n"
+          "&&&&&&&&.  %%%      #%%%%%%%%%%%%%%%%####(     ,###  %&&&&&&\n"
+          "&&&&&&     *%#%         %%%%%%%%%%%%#,...,,,...*##,,,,,#####\n"
+          "&&&&        ,%%#         .%%%%%%%%%%         .#%%        &&&\n"
+          "&&            %#%%        (%%%%%%%%         %%%%           #\n"
+          "                ##%#%      %%%%%%%%      %%%##              \n"
+          "                   %%%%%%%%%%%%%%%%%%##%%#%                 \n"
+          "                       /#%%%%%%%%%%%%%,                     \n")
     time.sleep(1)
     name = input("Please enter your name:- \n")
     time.sleep(1)
@@ -265,10 +296,23 @@ def load_next_scenario():
     Function to load next scenario from list "SCENARIOS".
     Will also finish game if all scenarios completed.
     """
+    print("\n+++++++++++++++++++++++++++++++++++++"
+          "+++++++++++++++++++++++++++++++++++++")
     global current_scenario_index
     current_scenario_index = current_scenario_index + 1
     if current_scenario_index == len(SCENARIOS):
-        print("You won!")
+        print("\n██     ██ ███████ ██      ██          \n"
+              "██     ██ ██      ██      ██          \n"
+              "██  █  ██ █████   ██      ██          \n"
+              "██ ███ ██ ██      ██      ██          \n"
+              " ███ ███  ███████ ███████ ███████     \n"
+              "\n"
+              "\n"
+              "██████   ██████  ███    ██ ███████ ██ \n"
+              "██   ██ ██    ██ ████   ██ ██      ██ \n"
+              "██   ██ ██    ██ ██ ██  ██ █████   ██ \n"
+              "██   ██ ██    ██ ██  ██ ██ ██         \n"
+              "██████   ██████  ██   ████ ███████ ██ \n")
         return
     else:
         scenario = SCENARIOS[current_scenario_index]
@@ -284,11 +328,11 @@ def show_scenario(scenario):
     selection = text_input()
     if selection == scenario["correct_choice"]:
         print(scenario["correct_answer"])
-        time.sleep(1)
+        time.sleep(3)
         load_next_scenario()
     else:
         print(scenario["wrong_answer"])
-        time.sleep(1)
+        time.sleep(3)
         game_over()
 
 
