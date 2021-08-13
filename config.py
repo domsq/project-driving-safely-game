a4539a06633f437e9b40f88ac9b2e581bf6aacea
domsq <Domsq@hotmail.com>
2021-08-13 16:50:39 +0000

    Move scenarios content from run.py into config.py

---
+++ config.py
@@ -0,0 +1,225 @@
+SCENARIOS = [
+  {
+    "question": "\n1. You start the drive to your holiday accommodation and " +
+                "after driving\nfor a few miles " +
+                "you encounter some slow traffic. " +
+                "\nThe driver behind you seems to be following a bit too " +
+                "closely\nwhat should you do?\n" +
+                "\nA: Slow down gradually, building more of a gap " +
+                "between you\nand the car in front\n" +
+                "\nB: Move over towards a position a bit left " +
+                "of the centre line of the road\n",
+    "correct_answer": "\n>Well done, this is the safe way to handle " +
+                      "the situation.",
+    "wrong_answer": "\n>Sorry, this is the wrong answer. " +
+                    "The driver behind attempts " +
+                    "\nto undertake you and collides " +
+                    "with you in the process.\n" +
+                    "\nRule #151 of the Highway Code " +
+                    "(pg. 50, 2015 Edition) states: " +
+                    "\nNever get so close to the vehicle in front " +
+                    "that you cannot stop safely.",
+    "correct_choice": "a"
+  },
+  {
+    "question": "\n2. The traffic improves but it starts to rain " +
+                "quite heavily." +
+                " You are \ntravelling behind another vehicle, how much of a" +
+                " time gap should you allow?\n" +
+                "\nA: Three Seconds\n" +
+                "\nB: Four Seconds\n",
+    "correct_answer": "\n>Correct - you need to allow extra distance on " +
+                      "slippery and wet roads.",
+    "wrong_answer": "\n>No, you have selected the wrong answer. " +
+                    "The vehicle ahead " +
+                    "brakes \nsuddenly and despite your best " +
+                    "efforts, you’re unable " +
+                    "to stop in time \nand collide with it.\n\n" +
+                    "Rule #227 of the Highway Code (pg. 76, 2015 " +
+                    "Edition) states: \n" +
+                    "In wet weather, stopping distances will be " +
+                    "at least double " +
+                    "those \nrequired for stopping on dry roads.",
+    "correct_choice": "b"
+  },
+  {
+    "question": "\n3. The weather improves and it becomes dry and warm. " +
+                "You are driving" +
+                "\non a 50mph road at the speed limit and you spot " +
+                "some stationary " +
+                "traffic \nup ahead. What’s the minimum safe distance to " +
+                "allow for stopping?\n" +
+                "\nA: 53m / 175 ft\n" +
+                "\nB: 36m / 120 ft\n",
+    "correct_answer": "\n>Well done, this is the average stopping " +
+                      "distance from 50 mph." +
+                      " Of course\nit’s always better to allow more " +
+                      "than you think.",
+    "wrong_answer": "\n>Sorry, this is not the right answer. You " +
+                    "misjudge the " +
+                    "distance to the \nvehicles ahead and are " +
+                    "unable to stop in " +
+                    "time and crash into the \nstationary traffic. \n" +
+                    "\nPg. 42 of the Highway Code (2015 Edition) shows " +
+                    "that the stopping \ndistance to allow when " +
+                    "travelling at 50mph " +
+                    "on a dry road in good \ncondition is at least " +
+                    "53m / 175 ft.",
+    "correct_choice": "a"
+   },
+  {
+    "question": "\n4. You are now travelling on a dual carriageway. " +
+                "There is " +
+                "a slow vehicle \nin front of you and you are " +
+                "considering overtaking" +
+                " them on the left. \nIs this allowed?\n" +
+                "\nA: Yes, it’s okay to overtake a slower vehicle in the " +
+                "right-hand lane of a \ndual carriageway\n" +
+                "\nB: No, you can only do this on a one-way street\n",
+    "correct_answer": "\n>Correct, it is okay to overtake on the left" +
+                      " when on a one-way street.",
+    "wrong_answer": "\n>Incorrect - as you attempt to pass the " +
+                    "vehicle the driver" +
+                    " has started \nmoving over to the left to allow " +
+                    "you past and" +
+                    " you crash into the other car.\n\n" +
+                    "Rule #268 of the Highway Code (pg. 89, 2015 " +
+                    "Edition) states:" +
+                    "\nWhen travelling on a motorway, do not overtake " +
+                    "on the left or" +
+                    " move \nto a lane on your left to overtake.",
+    "correct_choice": "b"
+    },
+  {
+    "question": "\n5. You are now driving through some countryside on a " +
+                "road that has " +
+                "some high \nhedges on either side of it." +
+                " You come up to a left-hand bend" +
+                " and are \nconcerned about the lack of visibility of " +
+                "the road ahead" +
+                " caused\nby the hedges. What should you watch out for?\n" +
+                "\nA: Pedestrians coming towards you\n" +
+                "\nB: No sign to warn of the bend\n",
+    "correct_answer": "\n>Correct, if the road has no footpaths " +
+                      "or pavements\n" +
+                      "pedestrians will need to walk facing traffic.",
+    "wrong_answer": "\n>This is not quite the right answer. You don’t " +
+                    "see some " +
+                    "pedestrians \napproaching and collide with them, " +
+                    "injuring them as a result.\n" +
+                    "\nRule #154 of the Highway Code (pg. 51, 2015 Edition) " +
+                    "advises to approach\ncountry roads with extra caution, " +
+                    "particularly when approaching\nbends as your " +
+                    "visibility of" +
+                    " pedestrians, etc. may be obscured. ",
+    "correct_choice": "a"
+    },
+  {
+    "question": "\n6. The road visibility improves but you need to " +
+                "make a right turn into a \nside road and have a " +
+                "queue of traffic " +
+                "behind you. Why is it a good idea to \ncheck your " +
+                "side mirror on " +
+                "the right before you turn?\n" +
+                "\nA: To see if anyone is trying to overtake you\n" +
+                "\nB: To see whether the side road is clear\n",
+    "correct_answer": "\n>Correct, you may have a driver trying to " +
+                      "overtake you, even " +
+                      "though \nthey should go around you on the left" +
+                      " if you’re " +
+                      "turning right...",
+    "wrong_answer": "\n>No, this is the wrong answer. You start turning and " +
+                    "out of nowhere" +
+                    "\na car who was trying to overtake you (even " +
+                    "though this " +
+                    "wouldn’t be safe \nto do) collides with you.\n" +
+                    "\nRule #180 of the Highway Code (pg. 62, 2015 " +
+                    "Edition) advises" +
+                    "\nto check your mirrors and blind spot to " +
+                    "ensure you’re not " +
+                    "being \novertaken before making the turn.",
+    "correct_choice": "a"
+    },
+  {
+    "question": "\n7. You notice that it has started raining again. " +
+                "You spot " +
+                "a hazard ahead \nthat you need to slow down for and " +
+                "attempt to brake " +
+                "but your \nanti-lock brakes don’t seem to be working. " +
+                "Your car starts to skid. \nWhat should you do next?\n"
+                "\nA: Use the handbrake to see if this helps\n"
+                "\nB: Release the brake pedal\n",
+    "correct_answer": "\n>Correct, you’d need to initially release the " +
+                      "brake pedal to stop the skid \nand then try " +
+                      "to carefully apply it again.",
+    "wrong_answer": "\n>Wrong, this causes your car to go into a sideways " +
+                    "slide and you \ncollide with another car.\n" +
+                    "\nRule #119 of the Highway Code (pg.38, 2015 " +
+                    "Edition) states: \nIf skidding occurs, remove the " +
+                    "cause by releasing the brake pedal fully \nor " +
+                    "easing off the accelerator. ",
+    "correct_choice": "b"
+    },
+  {
+    "question": "\n8. You start driving through a tunnel and notice what " +
+                "seems to be a \nburning smell coming from your car. The " +
+                "car isn’t showing any warning lights\nand still seems " +
+                "to be driveable, what should you do?\n" +
+                "\nA: Drive it out of the tunnel if you can \n" +
+                "\nB: Pull up, then walk to an emergency phone\n",
+    "correct_answer": "\n>This is the safest course of action, well done. " +
+                      "You get your car out \nof the tunnel, find a safe " +
+                      "place to pull over and contact your " +
+                      "roadside \nassistance company. ",
+    "wrong_answer": "\n>This isn’t the right thing to do... your car bursts " +
+                    "into flames \ncausing a serious issue. \n" +
+                    "\nRule #123 of the Highway Code (pg. 40, 2015 " +
+                    "Edition) states: \nDo not leave a parked vehicle " +
+                    "unattended with the engine running. ",
+    "correct_choice": "a"
+    },
+  {
+    "question": "\n9. You are back on the road again and you " +
+                "suddenly realise " +
+                "you’re travelling\nagainst the flow of traffic. What’s " +
+                "the best thing to do?\n" +
+                "\nA: Perform a three-point turn to get yourself facing " +
+                "the right way\n" +
+                "\nB: Pull into a side road and turn around\n",
+    "correct_answer": "\n>Yes, this would be the safest option. "
+                      "You turn off into a side road," +
+                      "\nget safely turned around and " +
+                      "continue on your way. ",
+    "wrong_answer": "\n>This is not the thing to do... while executing " +
+                    "a three-point " +
+                    "turn \na vehicle doesn’t see you in time and " +
+                    "collides with you.\n" +
+                    "\nRule #143 of the Highway Code (pg. 47, 2015 " +
+                    "Edition) states: \nYou must travel in the direction " +
+                    "of the road system and should choose \nthe " +
+                    "correct lane to exit ASAP.",
+    "correct_choice": "b"
+  },
+  {
+    "question": "\n10. You are nearly at your seaside destination and need " +
+                "to make a left turn \ninto a side road. What should you be " +
+                "watching out for most?\n" +
+                "\nA: Vehicles parked on either side\n" +
+                "\nB: Pedestrians\n",
+    "correct_answer": "\n>Correct, you’ll want to keep a watch for any " +
+                      "pedestrians. You notice \na pedestrian enter the road" +
+                      " as you turn down it and avoid them successfully.\n" +
+                      "\nYou have reached your destination and are ready " +
+                      "to begin your holiday!",
+    "wrong_answer": "\n>While you should always be on the lookout for other " +
+                    "vehicles, it’s \npedestrians you need to keep an eye " +
+                    "out for here. You fail to see a\npedestrian step into " +
+                    "the road and collide with them, causing them " +
+                    "\nsome moderate injuries. \n" +
+                    "\nRule #170 of the Highway Code (pg. 58, 2015 " +
+                    "Edition) states: \nPedestrians who are already " +
+                    "crossing a road into which you are "
+                    "turning\nhave right of way.",
+    "correct_choice": "b"
+  }
+]
